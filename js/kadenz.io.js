ff205c63a38ecc855f53a8028b884d7da4d944f0
Shin'ya Ueoka <qeexee@i-beam.org>
2014-02-22 10:08:24 +0900

    kadenz.io : replace event registration as adding callback
    Before the modification, the event callback method is registered as
    following:
      window.onload = function(e) {};
    By the above description, it is able to register only one callback
    method to catch the event.  So all event registration is replaced as
    adding a callback to keep availability as following:
      window.addEventListener("load", function(e){});

--- js/kadenz.io.js
+++ js/kadenz.io.js
@@ -1,15 +1,16 @@
-window.onload = function() {
+window.addEventListener("load", function(e){
   Kadenz.initPages();
-}
+});
 
-window.onkeypress = function(e) {
+window.addEventListener("keypress", function(e){
   if (e.charCode == "[".charCodeAt(0)) {
     Kadenz.skipToPrevPage();
   } else if (e.charCode == "]".charCodeAt(0)) {
     Kadenz.skipToNextPage();
   }
-}
-window.onkeydown = function(e) {
+});
+
+window.addEventListener("keydown", function(e){
   if (e.keyCode == 38) {        // Arrow up
     Kadenz.prevStep();
   } else if (e.keyCode == 40) { // Arrow down
@@ -19,17 +20,17 @@ window.onkeydown = function(e) {
   } else if (e.keyCode == 34) { // Page down
     Kadenz.skipToNextPage();
   }
-};
+});
 
-window.onclick = function(e) {
+window.addEventListener("click", function(e){
   Kadenz.nextStep();
-};
+});
 
-window.onhashchange = function(e) {
+window.addEventListener("hashchange", function(e){
   var hash = e.newURL.split("#");
   var num = Math.max(0, (~~hash[1]) - 1);
   if (num == -1) {
     return;
   }
   Kadenz.skipToPage(num);
-}
+});
